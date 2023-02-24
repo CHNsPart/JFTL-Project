a32b4b509b2d182a1242db0020ac3ae681627970
chnspart <imchn24@gmail.com>
2023-02-24 12:20:56 +0600

    server added

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 //import Image from 'next/image';
+"use client"
 import Landing from './components/Landing';
 import About from './components/About';
 import ShortNotes from './components/ShortNotes';
